075a5be0ecacd14d52dde47ec6d7407414ce32ed
spagaldi <57240852+spagaldi@users.noreply.github.com>
2021-01-15 22:12:27 -0800

    created stencil login page

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LogIn from './src/screens/LogInScreen.js';
+
 
 export default function App() {
   return (
+    /*
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    */
+    <LogIn />
   );
 }
 
---
+++ src/screens/LogInScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+} from 'react-native';
+
+
+const LogIn = ({ }) => {
+    return (
+        <View style={styles.container}>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#E6E7E9',
+    }
+});
+
+export default LogIn;
