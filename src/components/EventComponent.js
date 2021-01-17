06bde034c255453d5c45f0089e754b5802cae69b
jisoo kim <jisookim20032003@gmail.com>
2021-01-16 23:07:25 -0800

    changes to eventComponent.js

--- src/components/EventComponent.js
+++ src/components/EventComponent.js
@@ -15,85 +15,112 @@ import { Feather } from '@expo/vector-icons';
 import { TextInput } from 'react-native-gesture-handler';
 import DropDownPicker from 'react-native-dropdown-picker';
 import Icon from 'react-native-vector-icons/Feather';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const dimensions = Dimensions.get('window');
 const { width } = dimensions;
 const { height } = dimensions;
 
+const formatTime = (currentTime) =>{
+    var strTime = currentTime.getHours().toString();
+    var minute = currentTime.getMinutes();
+    var strMin = '';
+    if(minute < 10){
+        strTime = strTime + ":" + "0" + minute.toString();
+    }else{
+        strTime = strTime + ":" + minute.toString();
+    }
+    return strTime;
+}
+
 const EventComponent = ({}) => {
+    const[timeStart,setTimeStart] =  useState(new Date(2000,1,1,20,30,0,0));
+    const[showStart, setShowStart] = useState(false);
+    const[edittedStart,setEdittedStart] = useState(false);
+
+    const[timeStop,setTimeStop] =  useState(new Date(2000,1,1,20,30,0,0));
+    const[showStop, setShowStop] = useState(false);
+    const[edittedStop,setEdittedStop] = useState(false);
+    
+    const onChangeStart = (event,selectedValue)=>{
+        setShowStart(false);
+        setEdittedStart(true);
+        const selectedTime = selectedValue || new Date();
+        setTimeStart(selectedTime);
+    }
+
+    const onChangeStop = (event,selectedValue)=>{
+        setShowStop(false);
+        setEdittedStop(true);
+        const selectedTime = selectedValue || new Date();
+        setTimeStop(selectedTime);
+    }
+
     const [state, setState] = useState('');
         {/* COMPONENT for rectangle task*/}
         return (
-            <View style={{  
-                marginTop: 14,
-                height: height * 0.35,
-                width: width* 0.9,
-                borderRadius: 5,
-                backgroundColor: '#FFFFFF', }}>
+            <View style={styles.rectangle}>
         
                     {/* name */}
                     <View style ={{flexDirection: 'row', paddingTop: 14, }}> 
-                        <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Name: </Text>
+                        <Text style={{}}> Name: </Text>
                         <TextInput 
-                        style={{
-                            paddingLeft: width * 0.02,
-                            width: width * 0.57,
-                            backgroundColor: '#B4BCC3',
-                            borderRadius: 3,
-                            flexDirection: 'row',
-                            fontSize: 14,
-                            marginBottom: 15 }}
+                        style={styles.textInput}
                         returnKeyType="next"
                         />
                     </View> 
         
                     {/* date */}
                     <View style ={{flexDirection: 'row' }}> 
-                        <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Date: </Text>
+                        <Text style={styles.key}> Date: </Text>
                         <TextInput 
-                        style={{
-                            width: width * 0.57,
-                            backgroundColor: '#B4BCC3',
-                            borderRadius: 3,
-                            flexDirection: 'row',
-                            fontSize: 14,
-                            marginBottom: 15 }}
+                        style={styles.textInput}
                         returnKeyType="next"
                         />
                     </View>
         
                     {/* start time */}            
-                    <View style ={{flexDirection: 'row'  }}> 
-                        <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Start Time: </Text>
-                        <TextInput 
-                        style={{
-                            paddingLeft: width * 0.02,
-                            width: width * 0.2,
-                            backgroundColor: '#B4BCC3',
-                            borderRadius: 3,
-                            flexDirection: 'row',
-                            fontSize: 14,
-                            marginBottom: 15 }}
-                        returnKeyType="next"
-                        />
-                    </View> 
-        
+                    <View style ={{flexDirection: 'row', paddingBottom: 15}}> 
+                        <Text style={styles.key}> Start Time: </Text>
+                        {
+                            showStart && <DateTimePicker value={timeStart} 
+                            mode={'time'} 
+                            is24Hour={false} 
+                            display="default" 
+                            onChange={onChangeStart} /> 
+                        }
+                        <View style={styles.date}>
+                            <Text 
+                                style={{textAlign:'center'}} 
+                                onPress={()=>setShowStart(true)} >
+                                {formatTime(timeStart)}
+                            </Text>
+                        </View>
+                    </View>
+               
+                    
                     {/* Stop Time */}
-                    <View style ={{flexDirection: 'row', }}> 
-                        <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Stop Time: </Text>
-                        <TextInput 
-                        style={{
-                            paddingLeft: width * 0.02,
-                            width: width * 0.2,
-                            backgroundColor: '#B4BCC3',
-                            borderRadius: 3,
-                            flexDirection: 'row',
-                            fontSize: 14,
-                            marginBottom: 15 }}
-                        returnKeyType="next"
-                        />
-                    </View> 
-        
+                    <View style ={{flexDirection: 'row', paddingBottom: 15}}> 
+                        <Text style={styles.key}> Stop Time: </Text>
+                        {
+                            showStop && <DateTimePicker value={timeStop} 
+                            mode={'time'} 
+                            is24Hour={false} 
+                            display="default" 
+                            onChange={onChangeStop} /> 
+                        }
+                        <View style={styles.date}>
+                            <Text 
+                                style={{textAlign:'center'}} 
+                                onPress={()=>setShowStop(true)} >
+                                {formatTime(timeStop)}
+                            </Text>
+                        </View>
+                        
+                    </View>
+
+
+                    {/* Repeat */}
                     <View style ={{flexDirection: 'row', marginBottom: 15, alignContent:'center'}}> 
                         <Text style={{marginTop: 4, fontSize: 16, paddingHorizontal: 10,}}> Repeat: </Text>
                         <View style={{borderRadius:3, backgroundColor: '#B4BCC3'}}>
@@ -118,14 +145,7 @@ const EventComponent = ({}) => {
                     <View style ={{flexDirection: 'row', }}> 
                         <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10,}}> Repeat Until: </Text>
                         <TextInput 
-                        style={{
-                            paddingLeft: width * 0.02,
-                            width: width * 0.4,
-                            backgroundColor: '#B4BCC3',
-                            borderRadius: 3,
-                            flexDirection: 'row',
-                            fontSize: 14,
-                            marginBottom: 15 }}
+                        style={styles.textInput}
                         returnKeyType="next"
                         />
                     </View> 
@@ -133,4 +153,33 @@ const EventComponent = ({}) => {
         );
 }
 
+
+const styles = StyleSheet.create({
+    rectangle : {
+        marginTop: 14,
+        height: height * 0.35,
+        width: width* 0.9,
+        borderRadius: 5,
+        backgroundColor: '#FFFFFF',
+    },
+    textInput: {
+        paddingLeft: width * 0.02,
+        width: width * 0.57,
+        backgroundColor: '#B4BCC3',
+        borderRadius: 3,
+        flexDirection: 'row',
+        fontSize: 14,
+        marginBottom: 15 
+    },
+    key: {
+        marginTop: 3, fontSize: 16, paddingHorizontal: 10, 
+    },
+    date: {
+        marginLeft:10, 
+        backgroundColor:'#B4BCC3', 
+        width:width*0.20, 
+        alignContent:'center', 
+        justifyContent:'center', 
+        borderRadius: 3},
+});
 export default EventComponent;
