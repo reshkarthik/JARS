70a5c1a7e2d49d30a7bdcd18f03be1919e9ae2be
reshkarthik <reshmakarthik@Reshmas-MacBook-Pro-2.local>
2021-01-17 07:47:48 -0800

    login works

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,39 +3,38 @@ var userRef = db.ref().child("users");
 
 async function login(email, password){
     try {
-        let count = 0;
-        let foundUser;
+        let foundUser = -1;
         await userRef.on('value', (snapshot) => {
             const data = snapshot.val();
-            const allUsers = Object.keys(data).forEach(id => {
+            const allUsers = Object.keys(data).forEach(async id => {
                 var query = userRef.child(id).orderByKey();
-                query.once("value").then(function(snapshot) {
+                let count = 0;
+                await query.once("value").then(function(snapshot) {
                     snapshot.forEach(function(childshot){
                       var pkey = childshot.key; 
                       if (pkey == "email"){
                         const testEmail = childshot.val();
                         if (testEmail == email){
                             count += 1;
+                            foundUser = id;
+                            console.log("FOUND EMAIL " + testEmail);
                         }
                       }
                       else if (pkey == "password"){
                         const testPassword = childshot.val();
                         if (testPassword == password + 'calenJARS'){
                             count += 1;
-                            foundUser = id;
                         }
                       }
                     });
                 });
+                if (count == 2){
+                    console.log(foundUser)
+                    return foundUser;
+              }
               });
           });
-          if (count == 2){
-              console.log(foundUser)
-              return foundUser;
-        }
-          else{
-              return -1;
-          }
+    return foundUser;
     } catch (error) {
         console.log(error);
     }
