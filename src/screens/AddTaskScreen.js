3944b10da1f68561134c5e81c0297a930beefd68
jisoo kim <jisookim20032003@gmail.com>
2021-01-16 19:59:44 -0800

    put taskcomponents and eventcomponenets as separate files

--- src/screens/AddTaskScreen.js
+++ src/screens/AddTaskScreen.js
@@ -7,12 +7,17 @@ import {
     Dimensions,
     Button,
     ScrollView,
+    KeyboardAvoidingView,
     TouchableHighlight,
 } from 'react-native';
+
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {Picker} from '@react-native-picker/picker';
 import { Feather } from '@expo/vector-icons';
 import { TextInput } from 'react-native-gesture-handler';
+import BottomMenu from '../components/BottomBarComponent.js';
+import TaskComponent from '../components/TaskComponent.js'
+import EventComponent from '../components/EventComponent.js';
 
 const dimensions = Dimensions.get('window');
 const { width } = dimensions;
@@ -42,181 +47,20 @@ const AddTask = ({ navigation }) => {
         </View>
         <View style={styles.hairline} />
 
-
-
-
-
-
         {/* COMPONENT for rectangle task*/}
-        <View style={{  
-            marginTop: 14,
-            height: height * 0.18,
-            width: width* 0.9,
-            borderRadius: 5,
-
-            backgroundColor: '#FFFFFF', }}>
-            
-             {/* Name */}
-            <View style ={{flexDirection: 'row', paddingTop: 14, }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Name: </Text>
-                <TextInput 
-                style={{
-                    paddingLeft: width * 0.02,
-                    width: width * 0.57,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                />
-            </View> 
-
-             {/* Due date */}
-             <View style ={{flexDirection: 'row', }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Due Date: </Text>
-                <TextInput 
-                style={{
-                    paddingLeft: width * 0.02,
-                    width: width * 0.52,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                />
-            </View> 
-
-              {/* # of hours Need */}
-              <View style ={{flexDirection: 'row',  }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> # of Hours Needed: </Text>
-                <TextInput 
-                style={{
-                    paddingLeft: width * 0.02,
-                    width: width * 0.35,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                keyboardType="number-pad"
-                />
-            </View> 
-        </View>
+        <TaskComponent/>
 
         {/* COMPONENT for rectangle event*/}
-        <View style={{  
-        marginTop: 14,
-        height: height * 0.35,
-        width: width* 0.9,
-        borderRadius: 5,
-        backgroundColor: '#FFFFFF', }}>
-
-            {/* name */}
-            <View style ={{flexDirection: 'row', paddingTop: 14, }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Name: </Text>
-                <TextInput 
-                style={{
-                    paddingLeft: width * 0.02,
-                    width: width * 0.57,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                />
-            </View> 
-
-            {/* date */}
-            <View style ={{flexDirection: 'row' }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Date: </Text>
-                <TextInput 
-                style={{
-                    width: width * 0.57,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                />
-            </View>
-
-            {/* start time */}            
-            <View style ={{flexDirection: 'row'  }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Start Time: </Text>
-                <TextInput 
-                style={{
-                    paddingLeft: width * 0.02,
-                    width: width * 0.2,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                />
-            </View> 
-
-            {/* Stop Time */}
-            <View style ={{flexDirection: 'row', }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Stop Time: </Text>
-                <TextInput 
-                style={{
-                    paddingLeft: width * 0.02,
-                    width: width * 0.2,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                />
-            </View> 
-
-            <View style ={{flexDirection: 'row', }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Repeat: </Text>
-                <TextInput 
-                style={{
-                    paddingLeft: width * 0.02,
-                    width: width * 0.2,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                />
-            </View>
-
-            <View style ={{flexDirection: 'row', }}> 
-                <Text style={{marginTop: 3, fontSize: 16, paddingHorizontal: 10, }}> Repeat Until: </Text>
-                <TextInput 
-                style={{
-                    paddingLeft: width * 0.02,
-                    width: width * 0.4,
-                    backgroundColor: '#B4BCC3',
-                    borderRadius: 3,
-                    flexDirection: 'row',
-                    fontSize: 14,
-                    marginBottom: 15 }}
-                returnKeyType="next"
-                />
-            </View> 
-        </View>
-
-      
-
-
+        <EventComponent/>
 
            {/* Add Button; add an onpress action */}
         <TouchableHighlight style={styles.loginButtonWrapper}>
             <Button title=" ADD " color="#204969" />
         </TouchableHighlight>
 
+        <KeyboardAvoidingView style={styles.bottomMenu}>
+            <BottomMenu />
+        </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
@@ -259,6 +103,10 @@ const styles = StyleSheet.create({
         width: width * 0.25,
         justifyContent: 'center',
         marginBottom: 10,
+    },
+    bottomMenu: {
+        position: 'absolute',
+        bottom: 0,
     },
 });
 
